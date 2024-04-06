6edd7d98aecc2e02e1f35d345a20fb40df9ed57a
Priyanka Mondal <priyanka02010@gmail.com>
2024-04-05 22:05:15 -0700

    pbft updated with IORef for state

--- flame-runtime/playground/pBFT/Main.hs
+++ flame-runtime/playground/pBFT/Main.hs
@@ -50,231 +50,180 @@ def = 0
 
 type Node = String
 
-keys :: [Node]
-keys = ["A", "B", "C", "leader"]
-
-proxyToString :: forall a. KnownSymbol a => Proxy a -> String
-proxyToString _ = symbolVal (Proxy @a)
-
 times3 :: Int -> Int
 times3 x =  x*3 
 
-type State = String 
+type State = String  
+
+nextState :: State -> State
+nextState "INIT" = "PREPREPARE"
+nextState "PREPREPARE" = "PREPARE"
+nextState "PREPARE" = "COMMIT" 
+nextState "COMMIT" = "INIT"
+
+
+locOut :: forall a (loc ::LocTy). (KnownSymbol loc) => [a @ loc] -> [a] @ loc
+locOut as = wrap (helperOut as)
+
+helperOut :: forall a (loc ::LocTy). (KnownSymbol loc) => [a @ loc] -> [a]
+helperOut (a:as) = unwrap a : helperOut as  
+
 
 pbft :: Choreo IO () --forall (a:: LocTy). (KnownSymbol a) => Seq.StateT (SystemState a) IO NodeState --Choreo IO ()
 pbft = do 
   locAState <- locA `locally` \_ -> newIORef ("INIT" :: State)
   locBState <- locB `locally` \_ -> newIORef ("INIT" :: State)
-  locCState <- locA `locally` \_ -> newIORef ("INIT" :: State)
-  locLState <- locB `locally` \_ -> newIORef ("INIT" :: State)
+  locCState <- locC `locally` \_ -> newIORef ("INIT" :: State)
+  locLState <- leader `locally` \_ -> newIORef ("INIT" :: State)
 
   request <- client `locally` \_ -> do
       putStrLn "Client$ Input:"
       readLn :: IO Int
   
   req <- (client, request) ~> leader
-  st <- Seq.evalStateT (preprepare (req, leader, locA, locB, locC)) (startState, startReply)
-  
-  --printing local states at the end
-  sl <- leader `locally` \_ -> do 
-    let s' = fst st ! "leader"
-    putStrLn (show s') 
-    return s'
-
-  sa <- locA `locally` \_ -> do 
-    let s' = fst st ! "A"
-    putStrLn (show s') 
-    return s'
-
-  sb <- locB `locally` \_ -> do 
-    let s' = fst st ! "B"
-    putStrLn (show s') 
-    return s'
-
-  sc <- locC `locally` \_ -> do 
-    let s' = fst st ! "C"
-    putStrLn (show s') 
-    return s'
-
-  lc <- (leader, sl) ~> client
-  ac <- (locA, sa) ~> client 
-  bc <- (locB, sb) ~> client 
-  cc <- (locC, sc) ~> client 
-
--- client checking if there are 2f+1 matching results
-  ab <- client `locally` \un -> do compare_ [(un lc), (un ac), (un bc), (un cc)] 2
-  abc <- client `locally` \un -> do select_ (un ab)
-
-  c <- client `locally` \un -> do 
-    finalState <- wait $ un abc 
-    putStrLn $ "Client$ final state:" ++ (show finalState) 
-    return (finalState == COMMIT)
- 
+  (prepa, prepb, prepc) <- preprepare (req, leader, locLState, locA, locAState, locB, locBState, locC, locCState)
+  (ml, ma, mb, mc) <- prepare (leader, locLState, req) (locA, locAState, prepa) (locB, locBState, prepb) (locC, locCState, prepc) 
+  (repl, repa, repb, repc) <- commit (leader, locLState, ml) (locA, locAState, ma) (locB, locBState, mb) (locC, locCState, mc)
+  reply(leader, locLState, repl) (locA, locAState, repa) (locB, locBState, repb) (locC, locCState, repc)
 
   return () 
 
+preprepare :: forall (a:: LocTy) (b:: LocTy) (c :: LocTy) (l :: LocTy) m. 
+ (KnownSymbol a, KnownSymbol b, KnownSymbol c, KnownSymbol l) => 
+                     (Async Int @ l, Proxy l, IORef State @ l, Proxy a, IORef State @ a,
+                      Proxy b, IORef State @ b, Proxy c, IORef State @ c) 
+                  -> Choreo IO (Async Int @ a, Async Int @ b, Async Int @ c)
+preprepare (req, locl, statel, loca, statea, locb, stateb, locc, statec) =  do
+                           req' <- locl `locally` \un -> do  
+                            x <- wait (un req)  
+                            modifyIORef (un statel) nextState 
+                            return x
+                           prepa <-  (locl,req') ~> loca
+                           prepb <-  (locl, req') ~> locb  
+                           prepc <-  (locl, req') ~> locc
+                           return (prepa, prepb, prepc)
 
-nextState :: NodeState -> NodeState 
-nextState INIT = PREPREPARE
-nextState PREPREPARE = PREPARE
-nextState PREPARE = COMMIT 
-nextState COMMIT = COMMITTED
-nextState COMMITTED = INIT 
-
-updateState :: (Monad m) => String -> Int -> Seq.StateT ((HashMap (String) NodeState), (HashMap (String) (Int))) m (HashMap (String) NodeState, ((HashMap (String) (Int)))) 
-updateState locA x = do
-    current <- Seq.get
-    let state = fst current ! locA
-    let nextstate = nextState state 
-    let newState =  HM.insert locA nextstate (fst current)
-    Seq.put (newState, snd current)
-    return (newState, snd current)
-
-{--updateA :: Choreo IO () -- @ "A"
-updateA = do
-  locA `locally` \_ -> do 
-    case stateA
---}
-
-startState :: HM.HashMap (Node) NodeState
-startState = HM.fromList [(key, INIT) | key <- keys]
-
-startReply :: HM.HashMap (Node) Int
-startReply = HM.fromList [(key, def) | key <- keys]
-
-preprepare :: forall (a:: LocTy) (b:: LocTy) (c :: LocTy) (d :: LocTy) m. (KnownSymbol a, KnownSymbol b, KnownSymbol c, KnownSymbol d, Seq.MonadIO m) => 
-                     (Async Int @ a, Proxy a, Proxy b, Proxy c, Proxy d) 
-    -> Seq.StateT ((HashMap (String) NodeState), (HashMap (String) (Int))) (Choreo m) ((HashMap (String) NodeState), (HashMap (String) ( Int)))
-preprepare (req, locl, loca, locb, locc) =  do
-                           req' <- Seq.lift (locl `locally` \un -> do 
-                              x <-  Seq.liftIO $ wait (un req) 
-                              return x ) 
-                           prepa <- Seq.lift ((locl,req') ~> loca)
-                           prepb <- Seq.lift ((locl, req') ~> locb)  
-                           prepc <- Seq.lift ((locl, req') ~> locc)
-                           updateState (proxyToString locl) def 
-                            >> prepare (locl, req) (loca, prepa) (locb, prepb) (locc, prepc) 
-
-prepare :: forall (l:: LocTy) (a:: LocTy) (b:: LocTy) (c :: LocTy) m. (KnownSymbol l, KnownSymbol a, KnownSymbol b, KnownSymbol c, Seq.MonadIO m) => 
-                     (Proxy l, Async Int @ l) -> (Proxy a, Async Int @ a) ->  (Proxy b, Async Int @ b) ->  (Proxy c, Async Int @ c)  
-        ->  Seq.StateT ((HashMap (String) NodeState), (HashMap (String) ( Int))) (Choreo m) ((HashMap (String) NodeState), (HashMap (String) ( Int)))
-prepare (locl, req) (loca, msga) (locb, msgb) (locc, msgc) =  do
-                   {-- Seq.lift (locl `locally` \un -> do  
-                              Seq.liftIO $ putStrLn $ "prepare Leader:" ++ (show $ un req)
-                              return ())
-                    la <- Seq.lift ((locl, req) ~> loca)
-                    lb <- Seq.lift ((locl, req) ~> locb)
-                    lc <- Seq.lift ((locl, req) ~> locc)
-                    updateState (proxyToString locl) --}
-                    
-                    reqa <- Seq.lift (loca `locally` \un -> do 
-                              x <-  Seq.liftIO $ wait (un msga) 
-                              Seq.liftIO $ putStrLn $ "prepare A:" ++ (show x)
-                              return x )
-                    al <- Seq.lift ((loca, reqa) ~> locl)
-                    ab <- Seq.lift ((loca, reqa) ~> locb)
-                    ac <- Seq.lift ((loca, reqa) ~> locc)
-                    updateState (proxyToString loca) def
-
-                    reqb <- Seq.lift (locb `locally` \un -> do 
-                              x <-  Seq.liftIO $ wait (un msgb) 
-                              Seq.liftIO $ putStrLn $ "prepare B:" ++ (show x)
-                              return x )
-                    bl <- Seq.lift ((locb, reqb) ~> locl)
-                    ba <- Seq.lift ((locb, reqb) ~> loca)
-                    bc <- Seq.lift ((locb, reqb) ~> locc)
-                    updateState (proxyToString locb) def 
-
-                    reqc <- Seq.lift (locc `locally` \un -> do 
-                              x <-  Seq.liftIO $ wait (un msgc) 
-                              Seq.liftIO $ putStrLn $ "prepare C:" ++ (show x)
-                              return x )
-                    cl <- Seq.lift ((locc, reqc) ~> locl)
-                    ca <- Seq.lift ((locc, reqc) ~> loca)
-                    cb <- Seq.lift ((locc, reqc) ~> locb)
-                    updateState (proxyToString locc) def                           
-                      >> commit (locl, [req, al, bl, cl]) (loca, [msga, ba, ca]) (locb, [msgb, ab, cb]) (locc, [msgc, ac, bc])
-
-locOut :: forall a (loc ::LocTy). (KnownSymbol loc) => [a @ loc] -> [a] @ loc
-locOut as = wrap (helperOut as)
-
-helperOut :: forall a (loc ::LocTy). (KnownSymbol loc) => [a @ loc] -> [a]
-helperOut (a:as) = (unwrap a) : (helperOut as)  
-
-commit :: forall (l::LocTy) (a :: LocTy) (b :: LocTy) (c :: LocTy) m. 
-          (KnownSymbol l, KnownSymbol a, KnownSymbol b, KnownSymbol c, Seq.MonadIO m) => 
-          (Proxy l, [Async Int @ l]) -> (Proxy a, [Async Int @ a]) -> (Proxy b, [Async Int @ b]) -> (Proxy c, [Async Int @ c]) 
-          ->  Seq.StateT ((HashMap (String) NodeState), (HashMap (String) (Int))) (Choreo m) ((HashMap (String) NodeState), (HashMap (String) (Int)))
-commit  (locl, ls) (loca, as) (locb, bs) (locc, cs) =  do 
-    let outl = locOut ls  
-    repl <- Seq.lift (locl `locally` \un -> do 
-                              x <-  Seq.liftIO (selecT $ compare_ (un outl) 2)
-                              Seq.liftIO $ putStrLn $ "commit leader"
-                              return x )
-    updateState (proxyToString locl) def
-
-    let outa = locOut as  
-    repa <- Seq.lift (loca `locally` \un -> do 
-                              x <-  Seq.liftIO (selecT $ compare_ (un outa) 2)
-                              Seq.liftIO $ putStrLn $ "commit A"
-                              return x )
-    updateState (proxyToString loca) def
-
-    let outb = locOut bs  
-    repb <- Seq.lift (locb `locally` \un -> do 
-                              x <-  Seq.liftIO ( selecT $ compare_ (un outb) 2 )
-                              Seq.liftIO $ putStrLn $ "commit B"
-                              return x )
-    updateState (proxyToString locb) def
-
-    let outc = locOut cs  
-    repc <- Seq.lift (locc `locally` \un -> do 
-                              x <-  Seq.liftIO (selecT $ compare_ (un outc) 2)
-                              Seq.liftIO $ putStrLn $ "commit C"
-                              return x )
-    updateState (proxyToString locc) def
-     >> reply (locl, repl) (loca, repa) (locb, repb) (locc, repc)
-
-reply :: forall (l::LocTy) (a :: LocTy) (b :: LocTy) (c :: LocTy) m. 
-          (KnownSymbol l, KnownSymbol a, KnownSymbol b, KnownSymbol c, Seq.MonadIO m) => 
-          (Proxy l, Async Int @ l) -> (Proxy a, Async Int @ a) -> (Proxy b, Async Int @ b) -> (Proxy c, Async Int @ c)
-          ->  Seq.StateT ((HashMap (String) NodeState), (HashMap (String) (Int))) (Choreo m) ((HashMap (String) NodeState), (HashMap (String) (Int)))
-        --  -> Choreo IO ([Async Int] @ "client")
-reply (locl, repl) (loca, repa) (locb, repb) (locc, repc) = do 
-    repl' <-  Seq.lift (locl `locally` \un -> do 
-                              x <- Seq.liftIO (wait (un repl))
-                              Seq.liftIO $ putStrLn $ "reply leader"
-                              if (x /= failVal) then (return $ times3 x) else return (failVal) )
-    rl <-  Seq.lift $ (locl, repl') ~> client
-
-    repa <- Seq.lift(loca `locally` \un -> do 
-                              x <-  Seq.liftIO $ (wait $ un repa)
-                              Seq.liftIO $ putStrLn $ "reply A"
-                              if (x /= failVal) then (return $ times3 x) else return (failVal) )
-    ra <-  Seq.lift $ (loca, repa) ~> client
-
-    repb <- Seq.lift (locb `locally` \un -> do 
-                               x <- Seq.liftIO $ (wait $ un repb)
-                               Seq.liftIO $ putStrLn $ "reply B"
-                               if (x /= failVal) then (return $ times3 x) else return (failVal) )
-    rb <-  Seq.lift $ (locb, repb) ~> client
-
-    repc <- Seq.lift (locc `locally` \un -> do 
-                               x <-  Seq.liftIO $ (wait $ un repc)
-                               Seq.liftIO $ putStrLn $ "reply C"
-                               if (x /= failVal) then (return $ times3 x) else return (failVal) )
-    rc <-  Seq.lift $ (locc, repc) ~> client
-
-    let replies =  locOut [rl,ra,rb,rc]
-    replies <- Seq.lift $ client `locally` \un -> do 
-        answer <- Seq.liftIO $ selecT $ compare_ (un replies) 2
-        finalans <- Seq.liftIO $ wait answer
-        Seq.liftIO $ putStrLn $ "result at client:" ++ (show finalans)
+prepare :: forall (l :: LocTy) (a:: LocTy) (b:: LocTy) (c :: LocTy) m. (KnownSymbol l, KnownSymbol a, KnownSymbol b, KnownSymbol c) => 
+         (Proxy l, IORef State @ l, Async Int @ l)  
+      ->    (Proxy a, IORef State @ a, Async Int @ a) 
+      ->  (Proxy b, IORef State @ b ,Async Int @ b) 
+      ->  (Proxy c, IORef State @ c, Async Int @ c)  
+      ->  Choreo IO ([Async Int @ l], [Async Int @ a], [Async Int @ b], [Async Int @ c])
+prepare (locl, statel, req) (loca, statea, msga) (locb, stateb, msgb) (locc, statec, msgc) =  do
+                    reqa <-  loca `locally` \un -> do 
+                              x <-  wait (un msga) 
+                              modifyIORef (un statea) nextState 
+                              putStrLn $ "prepare A:" ++ show x
+                              return x 
+                    al <- (loca, reqa) ~> locl
+                    ab <- (loca, reqa) ~> locb
+                    ac <- (loca, reqa) ~> locc
     
-    updateState (proxyToString locl) def
-    updateState (proxyToString loca) def
-    updateState (proxyToString locb) def
-    updateState (proxyToString locc) def
+                    reqb <- locb `locally` \un -> do 
+                              x <- wait (un msgb) 
+                              modifyIORef (un stateb) nextState 
+                              putStrLn $ "prepare B:" ++ show x
+                              return x 
+                    bl <- (locb, reqb) ~> locl
+                    ba <- (locb, reqb) ~> loca
+                    bc <- (locb, reqb) ~> locc
+                  
+                    reqc <- locc `locally` \un -> do 
+                              x <- wait (un msgc) 
+                              modifyIORef (un statec) nextState 
+                              putStrLn $ "prepare C:" ++ show x
+                              return x
+                    cl <- (locc, reqc) ~> locl
+                    ca <- (locc, reqc) ~> loca
+                    cb <- (locc, reqc) ~> locb 
+
+                    return ([req, al, bl, cl], [msga, ba, ca], [msgb, ab, cb], [msgc, ac, bc])
+                
+                      -- >> commit (locl, [req, al, bl, cl]) (loca, [msga, ba, ca]) (locb, [msgb, ab, cb]) (locc, [msgc, ac, bc])
+
+
+commit :: forall (l::LocTy) (a :: LocTy) (b :: LocTy) (c :: LocTy). 
+          (KnownSymbol l, KnownSymbol a, KnownSymbol b, KnownSymbol c) => 
+             (Proxy l, IORef State @ l, [Async Int @ l]) 
+          -> (Proxy a, IORef State @ a, [Async Int @ a]) 
+          -> (Proxy b, IORef State @ b, [Async Int @ b]) 
+          -> (Proxy c, IORef State @ c, [Async Int @ c]) 
+          -> Choreo IO (Async Int @ l, Async Int @ a, Async Int @ b, Async Int @ c)
+commit  (locl, statel, ls) (loca, statea, as) (locb, stateb, bs) (locc, statec, cs) =  do 
+    let outl = locOut ls  
+    repl <- locl `locally` \un -> do 
+                              x <-  selecT $ compare_ (un outl) 2
+                              modifyIORef (un statel) nextState 
+                              putStrLn "commit leader:" -- ++ un statel
+                              return x 
+    
+    let outa = locOut as  
+    repa <- loca `locally` \un -> do 
+                              x <- selecT $ compare_ (un outa) 2
+                              modifyIORef (un statea) nextState 
+                              putStrLn "commit A:" -- ++ un statea
+                              return x 
+    
+    let outb = locOut bs  
+    repb <- locb `locally` \un -> do 
+                              x <-   selecT $ compare_ (un outb) 2 
+                              modifyIORef (un stateb) nextState 
+                              putStrLn  "commit B: :" -- ++ un stateb
+                              return x 
+  
+    let outc = locOut cs  
+    repc <- locc `locally` \un -> do 
+                              x <-  selecT $ compare_ (un outc) 2
+                              modifyIORef (un statec) nextState 
+                              putStrLn "commit C::" -- ++ un statec
+                              return x 
+    return (repl, repa, repb, repc)
+     -- >> reply (locl, repl) (loca, repa) (locb, repb) (locc, repc)
+
+reply :: forall (l::LocTy) (a :: LocTy) (b :: LocTy) (c :: LocTy). 
+          (KnownSymbol l, KnownSymbol a, KnownSymbol b, KnownSymbol c) => 
+          (Proxy l, IORef State @ l, Async Int @ l) 
+       -> (Proxy a, IORef State @ a, Async Int @ a) 
+       -> (Proxy b, IORef State @ b, Async Int @ b) 
+       -> (Proxy c, IORef State @ c, Async Int @ c)
+       -> Choreo IO ()
+reply (locl, statel, repl) (loca, statea, repa) (locb, stateb, repb) (locc, statec, repc) = do 
+    repl' <- locl `locally` \un -> do 
+                              x <- wait (un repl)
+                              modifyIORef (un statel) nextState
+                              putStrLn $ "reply leader:" -- ++ show $ un statel
+
+                              if (x /= failVal) then (return $ times3 x) else return (failVal) 
+    rl <-  (locl, repl') ~> client
+
+    repa <- loca `locally` \un -> do 
+                              x <-  (wait $ un repa)
+                              modifyIORef (un statea) nextState
+                              putStrLn $ "reply A:" -- ++ un statea
+                              if (x /= failVal) then (return $ times3 x) else return (failVal)
+    ra <- (loca, repa) ~> client
+
+    repb <- locb `locally` \un -> do 
+                               x <- (wait $ un repb)
+                               modifyIORef (un stateb) nextState
+                               putStrLn "reply B:" -- ++ un stateb
+                               if (x /= failVal) then (return $ times3 x) else return (failVal)
+    rb <- (locb, repb) ~> client
+
+    repc <- locc `locally` \un -> do 
+                               x <- (wait $ un repc)
+                               modifyIORef (un statec) nextState
+                               putStrLn "reply C:" -- ++ un statec
+                               if (x /= failVal) then (return $ times3 x) else return (failVal) 
+    rc <-  (locc, repc) ~> client
+
+    let replies =  locOut [rl,ra,rb,rc] -- [x @ loc] --> [x]
+    replies <- client `locally` \un -> do 
+        answer <- selecT $ compare_ (un replies) 2
+        finalans <- wait answer
+        putStrLn $ "result at client:" ++ (show finalans)
+    return ()
+    
 
 
 pBFTMain :: HttpConfig -> IO ()
